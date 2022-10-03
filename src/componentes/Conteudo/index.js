12b08cf295171e39940b7b32b93d4b56d2d5a90d
vitorfail <vitor_andrademanoel@hotmail.com>
2022-10-02 22:20:29 -0300

    123121

---
+++ src/componentes/Conteudo/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import React from 'react'
+
+export default function Conteudo(){
+    return(
+        <div className='conteudo'>
+            <img src='' alt="avatar"></img>
+            <h1>Mantenha o seu celular conectado</h1>
+            <h2>O WhatsApp conecta ao seus telefone para sincronizaar suas mensagens. Para reduzir o uso de dados, conecte seu telefone a uma rede Wi-Fi.</h2>
+        </div>
+    )
+}
--- src/componentes/LadoDireito/LadoDireito.js
+++ src/componentes/LadoDireito/LadoDireito.js
@@ -1,9 +1,10 @@
+import Conteudo from '../Conteudo'
 import './LadoDireito.css'
 
 export default function LadoDireito(){
     return(
         <div className='lado-direito'>
-
+            <Conteudo></Conteudo>
         </div>
     )
 }
